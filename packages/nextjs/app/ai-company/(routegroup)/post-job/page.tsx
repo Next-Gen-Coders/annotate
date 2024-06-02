7b82ab25699f5b03e491f9f7b8496ecd6616f44a
sandeep-prajapatii <sandeeprajapati18@gmail.com>
2024-06-02 18:54:11 +0530

    post-job-added

--- packages/nextjs/app/ai-company/(routegroup)/post-job/page.tsx
+++ packages/nextjs/app/ai-company/(routegroup)/post-job/page.tsx
@@ -1,29 +1,40 @@
+"use client";
+
 import { useState } from "react";
+import { parseEther } from "viem";
 import { Input } from "~~/components/@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~~/components/@/components/ui/select";
 import { Textarea } from "~~/components/@/components/ui/textarea";
+import { useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
 export default function Page() {
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [rawData, setRawData] = useState<File | null>(null);
   const [rawDataLink, setRawDataLink] = useState<string>("");
-  const [annotatorType, setAnnotatorType] = useState<string>("");
+  const [annotatorType, setAnnotatorType] = useState<number>(0);
   const [rewardPool, setRewardPool] = useState<string>("");
-
-  const handlePost = () => {
-    console.log("Title:", title);
-    console.log("Description:", description);
-    console.log("Raw Data:", rawData);
-    console.log("Annotator Type:", annotatorType);
-    console.log("Reward Pool:", rewardPool);
-  };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       setRawData(e.target.files[0]);
     } else {
       setRawData(null);
+    }
+  };
+
+  const { writeContractAsync } = useScaffoldWriteContract("DataMarketplace");
+
+  const handlePost = async () => {
+    console.log(title, description, rawDataLink, annotatorType, rewardPool);
+    try {
+      await writeContractAsync({
+        functionName: "postJob",
+        args: [title, description, rawDataLink, annotatorType],
+        value: parseEther(rewardPool),
+      });
+    } catch (e) {
+      console.error("Error setting greeting", e);
     }
   };
 
@@ -43,7 +54,7 @@ export default function Page() {
                 type="text"
                 placeholder="Title"
                 value={title}
-                onChange={(e) => setTitle(e.target.value)}
+                onChange={e => setTitle(e.target.value)}
               />
             </div>
             <div className="mb-4">
@@ -55,7 +66,7 @@ export default function Page() {
                 id="description"
                 placeholder="Description"
                 value={description}
-                onChange={(e) => setDescription(e.target.value)}
+                onChange={e => setDescription(e.target.value)}
               />
             </div>
             <div className="mb-4">
@@ -78,39 +89,34 @@ export default function Page() {
                 id="title"
                 type="text"
                 value={rawDataLink}
-                onChange={(e) => setRawDataLink(e.target.value)}
+                onChange={e => setRawDataLink(e.target.value)}
                 placeholder="xyz.com/upload/files"
               />
             </div>
             <div className="mb-4">
-              <p className="block font-semibold py-2">
-                Annotator Type
-              </p>
-              <Select>
+              <p className="block font-semibold py-2">Annotator Type</p>
+              <Select onValueChange={value => setAnnotatorType(parseInt(value))}>
                 <SelectTrigger className="w-full border border-[#98aecd] rounded-[10px]  px-3 py-4  bg-[#98aecd] bg-opacity-10">
-                  <SelectValue placeholder="Theme" />
+                  <SelectValue placeholder="Type" />
                 </SelectTrigger>
-                <SelectContent className="bg-[#161928] rounded-xl">
-                  <SelectItem value="light">Light</SelectItem>
-                  <SelectItem value="dark">Dark</SelectItem>
-                  <SelectItem value="system">System</SelectItem>
+                <SelectContent className="bg-[rgb(22,25,40)] rounded-xl">
+                  <SelectItem value="0">Novice</SelectItem>
+                  <SelectItem value="1">Pro</SelectItem>
+                  <SelectItem value="2">Elite</SelectItem>
+                  <SelectItem value="3">Titan</SelectItem>
                 </SelectContent>
               </Select>
             </div>
             <div className="mb-4">
-              <p className="block font-semibold py-2">
-                Reward Pool
-              </p>
-              <Select>
-                <SelectTrigger className="w-full border border-[#98aecd] rounded-[10px]  px-3 py-4  bg-[#98aecd] bg-opacity-10">
-                  <SelectValue placeholder="Theme" />
-                </SelectTrigger>
-                <SelectContent className="bg-[#161928] rounded-xl">
-                  <SelectItem value="light">Light</SelectItem>
-                  <SelectItem value="dark">Dark</SelectItem>
-                  <SelectItem value="system">System</SelectItem>
-                </SelectContent>
-              </Select>
+              <p className="block font-semibold py-2">Reward Pool</p>
+              <Input
+                className="border border-[#98aecd] rounded-[10px]  px-3 py-4  bg-[#98aecd] bg-opacity-10 focus:outline-none focus:shadow-outline"
+                id="rewardPool"
+                type="number"
+                placeholder="Reward amount"
+                value={rewardPool}
+                onChange={e => setRewardPool(e.target.value)}
+              />
             </div>
             <div className="flex items-center justify-center w-full py-6">
               <button
